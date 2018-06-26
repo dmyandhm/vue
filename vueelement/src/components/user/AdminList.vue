3d9be944dd6a2dee261d514839787f7c9b001d9a
mydai <1922159124@qq.com>
2018-06-26 17:40:17 +0800

    新增模态框

--- vueelement/src/components/user/AdminList.vue
+++ vueelement/src/components/user/AdminList.vue
@@ -17,6 +17,9 @@
           <el-form-item>
             <el-button type="primary" @click="handleSearch">查询</el-button>
           </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="showAddDialog">新增</el-button>
+          </el-form-item>
         </el-form>
       </el-col>
 
@@ -42,6 +45,28 @@
                      layout="total, sizes, prev, pager, next, jumper" :total="total">
       </el-pagination>
 
+      <!--新增界面-->
+      <el-dialog title="新增" :visible.sync ="addFormVisible" :close-on-click-modal="false">
+        <el-form :model="addForm" label-width="80px" :rules="addFormRules" ref="addForm">
+          <el-form-item label="姓名" prop="name">
+            <el-input v-model="addForm.name" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="账号" prop="username">
+            <el-input v-model="addForm.username" auto-complete="off"></el-input>
+          </el-form-item>
+          <!--<el-form-item label="日期">
+            <el-date-picker type="date" placeholder="选择日期" v-model="addForm.publishAt"></el-date-picker>
+          </el-form-item>
+          <el-form-item label="简介" prop="description">
+            <el-input type="textarea" v-model="addForm.description" :rows="8"></el-input>
+          </el-form-item>-->
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click.native="addFormVisible = false">取消</el-button>
+          <el-button type="primary" @click.native="addSubmit" :loading="addLoading">提交</el-button>
+        </div>
+      </el-dialog>
+
     </el-col>
   </el-row>
 </template>
@@ -62,6 +87,22 @@
         total: 0,
         page: 1,
         limit: 10,
+
+        //新增相关数据
+        addFormVisible: false,//新增界面是否显示
+        addLoading: false,
+        addFormRules: {
+          name: [
+            {required: true, message: '请输入姓名', trigger: 'blur'}
+          ],
+          username: [
+            {required: true, message: '请输入账户', trigger: 'blur'}
+          ]
+        },
+        addForm: {
+          name: '',
+          username: ''
+        }
       }
     },
     methods: {
@@ -103,7 +144,31 @@
       handleCurrentChange(val) {
         this.page=val;
         this.search();
-      }
+      },
+      //显示新增页面
+      showAddDialog: function () {
+        this.addFormVisible = true;
+//        this.addForm = {
+//          name:
+        this.$refs['a \'\',\n' +
+        '//          username: \'\'\n' +
+        '//        };ddForm'].resetFields();
+      },
+      //新增
+      addSubmit: function () {
+        let that = this;
+        this.$refs.addForm.validate((valid) => {
+          if (valid) {
+//            that.loading = true;
+            that.$message.success({showClose: true, message: '新增成功', duration: 2000});
+            that.addFormVisible = false;
+            that.handleSearch();
+
+          }
+        });
+      },
+
+
     },
     mounted() {
       this.handleSearch()
